769c89c145a134fe9c1580c0a4334e8fc47ba65f
P-Slark <30462011+P-Slark@users.noreply.github.com>
2020-07-23 18:16:45 +0800

    Update 0002. 01背包问题.cpp

--- 0002. 01背包问题.cpp
+++ 0002. 01背包问题.cpp
@@ -1,3 +1,4 @@
+//二维
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 1005
@@ -18,5 +19,27 @@ int main(){
         }
     }
     cout<<dp[N][V]<<endl;
+    return 0;
+}
+
+//一维
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1005
+int v[MAX];
+int w[MAX];
+int main(){
+    int N,V;
+    cin>>N>>V;
+    for(int i = 1;i <= N;i++)
+        cin>>w[i]>>v[i];
+    vector<int> dp(V+1,0);
+    for(int i = 1;i<N+1;i++){
+        for(int j = V;j>=1;j--){
+            if(j>=w[i])
+                dp[j] = max(dp[j-w[i]]+v[i],dp[j]);
+        }
+    }
+    cout<<dp[V]<<endl;
     return 0;
 }
